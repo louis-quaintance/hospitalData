ced7e6b94bcfc4b19ee0c8691d437a8b5377d77f
Louis Quaintance <louis.quaintance@first-utility.com>
2014-07-20 20:20:58 +0100

    conintued dev

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,222 @@
-
+window.addEventListener('load', function() {
+    FastClick.attach(document.body);
+}, false);
 
 //cordova emulate ios --target="iPad" 
 //**, HospitalDataProcessor, HospitalAppCache*/
+//count-to="{{procedureStat.frequency | number:0}}" value="0" duration="3"
+
+angular.module('HospitalDataApp', ['countTo', "angucomplete"]).controller('HospitalDataController', ['$scope', '$http', function($scope, $http) {
+
+
+    function cache(data){
+
+        function populateDB(tx) {
+             tx.executeSql('DROP TABLE IF EXISTS CACHEDDATA');
+             tx.executeSql('CREATE TABLE IF NOT EXISTS CACHEDDATA (id INTEGER PRIMARY KEY, dateOfCache TEXT, hospitalName TEXT, trust TEXT, description TEXT, opcs TEXT, hrg TEXT, tariffNow REAL, tariffForecast REAL, frequency INTEGER, frequencyForecast INTEGER, revenue REAL, revenueForecast REAL, costOfConsumerablesNow REAL, costOfConsumerablesForecast REAL)');
+             
+             var count = 0;
+
+             var dateOfCache = moment().format("YYYY[-]MM[-]DD");
+
+             for(var hospitalName in data){
+
+                data[hospitalName].forEach(function(record){
+                   
+                    tx.executeSql('INSERT INTO CACHEDDATA (id, dateOfCache, hospitalName,trust,description,opcs,hrg,tariffNow,tariffForecast,frequency,frequencyForecast,revenue,revenueForecast,costOfConsumerablesNow,costOfConsumerablesForecast)' + 
+                        'VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [count, dateOfCache, hospitalName, hospitalName, record.description, record.opcs,record.hrg,parseFloat(record.tariffNow),
+                        parseFloat(record.tariffForecast),parseInt(record.frequency),parseInt(record.frequencyForecast),
+                        parseFloat(record.revenue),parseFloat(record.revenueForecast),
+                        parseFloat(record.costOfConsumerablesNow),parseFloat(record.costOfConsumerablesForecast)]);
+
+                    count++;
+
+                });
+             }
+        }
+
+        function errorCB(err) {
+            alert("Error processing SQL: "+err.message);
+        }
+
+        function successCB() {
+            
+        }
+
+        openDb().transaction(populateDB, errorCB, successCB);
+    }
+
+    function isDataCached(httpFetchCallback){
+
+        openDb().transaction(function(tx){
+
+            tx.executeSql('SELECT DISTINCT dateOfCache FROM CACHEDDATA', [], function(tx, results){
+
+                for(var i = 0; i < results.rows.length; i++){
+
+                    var row = results.rows.item(i);
+    
+                    if(row.dateOfCache === moment().format("YYYY[-]MM[-]DD")){
+                        bindModelToView();
+                    } else {
+                        httpFetchCallback();
+                    }
+
+                    break;
+                }
+            })
+        });
+    }
+
+    function lookupHospitalNames(){
+
+        openDb().transaction(function(tx){
+
+            tx.executeSql('SELECT DISTINCT hospitalName FROM CACHEDDATA', [], function(tx, results){
+
+                var hospitalNames = [];
+
+                for(var i = 0; i < results.rows.length; i++){
+                    hospitalNames.push({ name: results.rows.item(i).hospitalName })
+                }
+
+                $scope.hospitals = hospitalNames;
+
+
+                    $scope.$apply();
+
+            })
+        });
+    }
+
+    function openDb(){
+        return window.openDatabase("HospitalData", "1.0", "HospitalData", 200000);
+    }
+
+    function bindModelToView(){
+
+        var querySuccess = function(tx, results){
+
+            var hospitalNameList = [];
+            var firstHospitalSet = false;
+
+            var hospitalName = results.rows.item(0).hospitalName;  
+
+            var hospitalRows = [];
+
+            for(var i = 0; i < results.rows.length; i++){
+
+                var row = results.rows.item(i);
+
+                if(row.hospitalName != hospitalName){
+                    break;
+                }
+                hospitalRows.push(row);
+            }
+
+            $scope.hospitalName = hospitalName;
+
+                    $scope.procedureStats = hospitalRows;
+                    
+
+                    lookupTotals(hospitalName, $scope);
+
+                    lookupHospitalNames();
+
+                    $scope.$apply();
+
+                    resetSlider();  
+        };       
+
+        
+
+        function queryDB(tx){
+            tx.executeSql('SELECT * FROM CACHEDDATA order by hospitalName', [], querySuccess, errorCB);
+        }
+
+        openDb().transaction(queryDB, errorCB);
+    }
+
+    function errorCB(err) {
+            alert("Error processing SQL: " + err.message);
+        }
+
+    function resetSlider(){
+            setTimeout(function(){
+                        $('.bxslider').bxSlider({
+                            pager: false
+                        });
+                }, 1500);
+        }
+
+    function lookupTotals(hospitalName, $scope){
+
+        openDb().transaction(function(tx){
+
+            tx.executeSql('SELECT SUM(frequency) as f, SUM(frequencyForecast) as ff, SUM(revenue) as r, SUM(revenueForecast) as rf, SUM(costOfConsumerablesNow) as ccn, SUM(costOfConsumerablesForecast) as ccf FROM CACHEDDATA where hospitalName = ?', [hospitalName], function(tx, results){
+
+                for(var i = 0; i < results.rows.length; i++){
+
+                    var row = results.rows.item(i);
+
+                    $scope.overallRevenue = row.r;
+                    $scope.overallRevenueForecast = row.rf;
+
+                    $scope.overallConsumerables = row.ccn;
+                    $scope.overallConsumerablesForecast = row.ccf;
+
+                    $scope.netRevenue = row.r - row.ccn;
+                    $scope.netRevenueForecast = row.rf - row.ccf;
+
+                    $scope.totalNumberOfProcs = row.f;
+                    $scope.totalNumberOfProcsForecast = row.ff;
+
+                    $scope.upliftInIncome = $scope.netRevenueForecast - $scope.netRevenue
+                    $scope.upliftInIncomePercentage = ($scope.upliftInIncome * 100) / $scope.netRevenue;
+
+                    $scope.$apply();
+
+                    $( ".barmore" ).animate({
+                        width: (row.f * 100 / (row.ff + row.f)) + "%"
+                      }, 3000, function() {
+                        $( ".barless" ).animate({
+                        width: (row.ff * 100 / (row.ff + row.f)) + "%"
+                      }, 3000, function() {
+                        // Animation complete.
+                      });
+                      });
+
+                    
+
+                }
+
+            }, errorCB);
+
+
+        }, errorCB);
+    }
+
+
+
+    
+
+    isDataCached(function(){
+
+        $http({method: 'GET', url: 'http://localhost:8111/'}).
+            success(function(data, status, headers, config) {
+
+                cache(data.data);
+
+                bindModelToView();                 
+            }).
+            error(function(data, status, headers, config) {
+              alert("todo")
+            }
+        );
+
+    });
+
+}]);
 
 
 var aModel = {
@@ -106,29 +321,3 @@ var aModel = {
         }]
     }
 };
-
-angular.module('HospitalDataApp', ['countTo']).controller('HospitalDataController', ['$scope', function($scope) {
-
-    $scope.hospitalName = "Ealing";
-
-    $scope.procedureStats = aModel.data.Ealing;
-
-    $scope.overallRevenue = 100;
-    $scope.overallRevenueForecast = 120;
-
-    $scope.overallConsumerables = 200;
-    $scope.overallConsumerablesForecast = 210;
-
-    $scope.netRevenue = 800;
-    $scope.netRevenueForecast = 900;
-
-    $scope.upliftInIncome = 100;
-    $scope.upliftInIncomePercentage = 10;
-
-    setTimeout(function(){
-            $('.bxslider').bxSlider({
-                pager: false
-            });
-    }, 1500);
-
-}]);
